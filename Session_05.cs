61148eba43ed00d8c69cae078ed439e9bc78ea17
Administrator <Administrator@N301PC08>
2024-10-30 22:33:06 +0700

    B2

---
+++ Session_05.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DINHDUCMANH
+{
+    internal class Session_05
+    {
+        public static void swap(ref int a, ref int b)
+        {
+            int temp = a; a = b; b = temp;
+        }
+        public static void Main()
+        {
+            int a = 6; int b = 7;
+            Console.WriteLine($"before call a= {a}, b= {b}");
+            swap(ref a, ref b); Console.WriteLine($"after call a={a} b={b}");
+            Console.ReadKey();
+        }
+    }
+
+    class Game_taixiu
+    {
+        static int rollDice()
+        {
+            Random rnd = new Random();
+            int die_1 = rnd.Next(6) + 1;
+            int die_2 = rnd.Next(6) + 1;
+            int die_3 = rnd.Next(6) + 1;
+            int sum_of_dice = die_1 + die_2 + die_3;
+            return sum_of_dice;
+
+        }
+        public static void PlayOneround()
+        { int com_dice = rollDice();
+            Console.WriteLine("ban doan tai hay xiu <t/x>");
+            String User_gussing = Console.ReadLine();
+            if (User_gussing.ToUpper().Equals("T"))
+            {
+                if (com_dice >= 10)
+                    Console.WriteLine("ban thang");
+                else Console.WriteLine("ban thua");
+
+            }
+            else if (User_gussing.ToUpper().Equals("X"))
+            {
+                if (com_dice <= 10)
+                    Console.WriteLine("ban thang");
+                else
+                    Console.WriteLine("ban thua");
+
+            }
+            else
+            { Console.WriteLine("Vui long chon dung <t/x>");
+            }
+        }
+        public static void game_engine()
+        {
+            do
+            {
+                PlayOneround();
+                Console.WriteLine("ban co muon tiep tuc? <c/k>");
+                string choice = Console.ReadLine();
+                if (choice.ToUpper().Equals("K"))
+                    break;
+            }
+            while (true);
+            Console.WriteLine("99% so nguoi bo cuoc khi sap thang");
+        }
+        public static void Main()
+        {
+            game_engine();
+    }
+    }
+}
+
+
+
+            
